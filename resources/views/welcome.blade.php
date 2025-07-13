bd202988df11605c445ae0ca1b0ef4d38491ab97
kurob <kurob1993@gmail.com>
2025-07-14 00:06:01 +0700

    add: tombol info iuran

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,7 @@
             </p>
             <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center">
                 <div class="rounded-md shadow">
-                    <a href="#"
+                    <a href="https://wa.me/628999702143?text=info-iuran"
                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
                         Info Tagihan
                     </a>
