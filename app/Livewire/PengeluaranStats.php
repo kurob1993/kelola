59c20af4bbe1cc8c157dc672c10b058d66d32f8e
kurob <kurob1993@gmail.com>
2025-07-16 05:34:12 +0700

    fix: dashboard card pengeluaran perbulan

--- app/Livewire/PengeluaranStats.php
+++ app/Livewire/PengeluaranStats.php
@@ -2,11 +2,12 @@
 
 namespace App\Livewire;
 
+use App\Models\TransaksiPengeluaranDetail;
+use Carbon\Carbon;
 use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Number;
-use Illuminate\Support\Str;
 
 class PengeluaranStats extends BaseWidget
 {
@@ -14,38 +15,56 @@ class PengeluaranStats extends BaseWidget
 
     protected function getStats(): array
     {
+        $bulan = $this->filters['bulan'] ?: now()->format('m');
+        $tahun = $this->filters['tahun'] ?: now()->format('Y');
+
+        // $bulanNama = Carbon::createFromDate(null, $bulan, 1)->translatedFormat('F');
+
+        $pengeluaran = TransaksiPengeluaranDetail::whereHas('transaksi', function ($query) use ($bulan, $tahun) {
+            return $query->whereMonth('tanggal', $bulan)->whereYear('tanggal', $tahun);
+        })->get();
+
+        $total = $pengeluaran->sum(fn($item) => $item->jumlah * $item->qty);
+        $rataRata = $total/$pengeluaran->count();
+
+        // Mencari transaksi dengan nilai terbesar
+        $terbanyak = $pengeluaran->sortByDesc(fn($item) => $item->jumlah * $item->qty)->first();
+        // Atau jika hanya ingin nominalnya saja
+        $nominalTerbanyak = $pengeluaran->max(fn($item) => $item->jumlah * $item->qty);
+        $tanggalTerbanyak = $terbanyak?->transaksi ? Carbon::make($terbanyak?->transaksi->tanggal)->format('d F Y') : null;
+
+        $jumlah = $pengeluaran->count();
+
+
+
         return [
             Stat::make('Total Pengeluaran', Number::currency(
-                number: 200000,
+                number: $total,
                 in: 'Rp.',
                 precision: 0,
             ))
                 ->color('danger')
-                ->icon('heroicon-o-chart-pie'),
+                ->icon('heroicon-o-banknotes'),
 
             Stat::make('Rata-rata Harian', Number::currency(
-                number: 50000000,
+                number: $rataRata,
                 in: 'Rp.',
                 precision: 0,
             ))
                 ->color('danger')
-                ->icon('heroicon-o-chart-pie'),
+                ->icon('heroicon-o-chart-bar'),
 
-            Stat::make('Pengeluaran Terbanyak', Number::currency(
-                number: 50000000,
+            Stat::make('Terbanyak ('.$tanggalTerbanyak.')', Number::currency(
+                number: $nominalTerbanyak,
                 in: 'Rp.',
                 precision: 0,
             ))
                 ->color('danger')
-                ->icon('heroicon-o-chart-pie'),
+                ->icon('heroicon-o-fire'),
 
-            Stat::make('Jumlah Transaksi', Number::currency(
-                number: 50000000,
-                in: 'Rp.',
-                precision: 0,
-            ))
+            Stat::make('Jumlah Transaksi', $jumlah . ' transaksi')
                 ->color('danger')
-                ->icon('heroicon-o-chart-pie'),
+                ->icon('heroicon-o-clipboard-document-list'),
         ];
     }
 }
